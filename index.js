11a4aa763e30652b872c5c5ee60c3800e4672027
Rehan-ahmed-459 <ahmedrehan549@gmail.com>
2023-05-02 21:36:22 +0530

    Bot Status Updated

--- index.js
+++ index.js
@@ -1,43 +1,106 @@
-const Telegraf = require('telegraf');
+const {Telegraf} = require('telegraf');
+const { Extra, Markup } = Telegraf;
 const vtApi = require('virustotal-api');
+const axios =require('axios');
 const fs = require('fs');
-
+const FormData = require('form-data');
+const path = require('path');
+require('dotenv').config()
 // Set up the Telegraf bot
-const bot = new Telegraf(process.env.BOT_TOKEN);
+const token=process.env.BOT_TOKEN;
+const bot = new Telegraf(`${token}`);
 
 // Set up the VirusTotal API client
-const vt = new vtApi(process.env.VT_API_KEY);
-
+const vt = new vtApi(process.env.VIRUSTOTAL_API_KEY);
 // Set up the bot's command
-bot.command('scan', (ctx) => {
-  // Check if the user sent a file
-  if (!ctx.message.document) {
-    ctx.reply('Please send me a file to scan.');
-    return;
-  }
+bot.command('start', ctx => {
+    console.log(ctx.from)
+    bot.telegram.sendMessage(ctx.chat.id, 'Hello there! Welcome to Virus Total Bot Created By 0x01Ahmed😎\n\nType /menu to List the Available Commands\n', {
+    })
+})
+bot.command('menu', ctx => {
+    console.log(ctx.from)
+    bot.telegram.sendMessage(ctx.chat.id, '\n/start - To restart the Bot\n /scan to Scan an File', {
+    })
+})
+bot.command('scan', ctx => {
+    console.log(ctx.from)
+    bot.telegram.sendMessage(ctx.chat.id, '\nSend a File to Scan📂', {
+    })
+})
+
+
 
   // Download the file from Telegram
-  const fileId = ctx.message.document.file_id;
-  const filePath = `${__dirname}/${fileId}.pdf`;
-  ctx.telegram.getFileLink(fileId).then((link) => {
-    const fileStream = fs.createWriteStream(filePath);
-    request(link).pipe(fileStream).on('close', () => {
-      // Scan the file with VirusTotal
-      vt.fileScan(filePath, (err, res) => {
-        if (err) {
-          console.error(err);
-          ctx.reply('Sorry, something went wrong.');
-          return;
+  
+  bot.on('document', async (ctx) => {
+    const fileId = ctx.update.message.document.file_id;
+    const fileUrl = await ctx.telegram.getFileLink(fileId);
+  
+    const file = await axios.get(fileUrl.href, { responseType: 'stream' });
+    const formData = new FormData();
+    formData.append('file', file.data);
+  
+    const headers = {
+      'x-apikey': process.env.VIRUSTOTAL_API_KEY,
+      ...formData.getHeaders(),
+    };
+  
+    const vtResponse = await axios.post(
+      'https://www.virustotal.com/api/v3/files',
+      formData,
+      {
+        headers: headers,
+      }
+    );
+  
+    const resourceId = vtResponse.data.data.id;
+    const analysisUrl = `https://www.virustotal.com/api/v3/analyses/${resourceId}`;
+    const analysisHeaders = {
+      'x-apikey': process.env.VIRUSTOTAL_API_KEY,
+      accept: 'application/json',
+    };
+  
+    const checkFile = setInterval(async () => {
+      try {
+        const analysisResponse = await axios.get(analysisUrl,{
+          headers: analysisHeaders,
+        });
+        
+  
+        if (analysisResponse.data.data.attributes.status === 'completed') {
+          clearInterval(checkFile);
+  
+          const stats = analysisResponse.data.data.attributes.stats;
+          const harmfulEngines = stats.harmful || 0;
+          const harmlessEngines = stats.harmless || 0;
+          const undetectedEngines = stats.undetected || 0;
+          const totalEngines = harmfulEngines + harmlessEngines + undetectedEngines;
+  
+          let result = 'No threats detected!';
+          if (harmfulEngines > 0) {
+             result = `⚠️ WARNING! This file is malicious according to VirusTotal.\n\n` +
+                     `Detected by ${analysisResponse.data.data.attributes.stats.malicious} out of ${analysisResponse.data.data.attributes.stats.undetected} engines.\n\n` +
+                    `Malicious engines: ${maliciousEngines.join(', ')}`;
+                    ctx.reply(result);
+          } else if (undetectedEngines > 0) {
+            result = `✅ This file is safe according to VirusTotal.\n\n` +
+                     `Detected by ${analysisResponse.data.data.attributes.stats.harmless} out of ${analysisResponse.data.data.attributes.stats.undetected} engines.`;
+                     ctx.reply(result);
+          }
+          
+          
         }
-        const permalink = res.permalink;
-        const positives = res.positives;
-        const total = res.total;
-        const message = `Scan results: ${positives}/${total} detected. More details at: ${permalink}`;
-        ctx.reply(message);
-      });
-    });
+      } catch (error) {
+        console.error(error);
+      }
+    }, 5000);
+  
+    ctx.reply(
+      `Your File is Beign scanned.....🕵️‍♂️🕤`
+    );
   });
-});
+
 
 // Start the bot
 bot.startPolling();
